1be775494218a84c4718c5ea932610d33ffda36f
dothuong <dothuong2k1@gmail.com>
2023-09-14 16:22:48 +0700

    ignore ts err

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
-import React from "react";
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { fetchUser } from "@/lib/actions/user.action";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Image from "next/image";
-import { profileTabs } from "@/constants";
-import { TabsContent } from "@radix-ui/react-tabs";
-import ThreadTabs from "@/components/shared/ThreadTabs";
-const Page = async ({ params }: { params: { id: string } }) => {
-  const user = await currentUser();
 
+import { profileTabs } from "@/constants";
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { fetchUser } from "@/lib/actions/user.action";
+
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(params.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
@@ -29,13 +27,21 @@ const Page = async ({ params }: { params: { id: string } }) => {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+
       <div className="mt-9">
         <Tabs defaultValue="threads" className="w-full">
           <TabsList className="tab">
             {profileTabs.map((tab) => (
               <TabsTrigger key={tab.label} value={tab.value} className="tab">
-                <Image src={tab.icon} alt={tab.label} width={24} height={24} />
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
                 <p className="max-sm:hidden">{tab.label}</p>
+
                 {tab.label === "Threads" && (
                   <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
                     {userInfo.threads.length}
@@ -50,7 +56,8 @@ const Page = async ({ params }: { params: { id: string } }) => {
               value={tab.value}
               className="w-full text-light-1"
             >
-              <ThreadTabs
+              {/* @ts-ignore */}
+              <ThreadsTab
                 currentUserId={user.id}
                 accountId={userInfo.id}
                 accountType="User"
@@ -61,6 +68,5 @@ const Page = async ({ params }: { params: { id: string } }) => {
       </div>
     </section>
   );
-};
-
+}
 export default Page;
