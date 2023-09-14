2b66a3ab34bc942f52e2b549a18d9f5271d74757
dothuong <dothuong2k1@gmail.com>
2023-09-14 22:51:11 +0700

    fix content right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Topbar } from "@/components/shared/Topbar";
 import { LeftSidebar } from "@/components/shared/LeftSidebar";
-import { RightSidebar } from "@/components/shared/RightSidebar";
 import { Bottombar } from "@/components/shared/Bottombar";
 import "../globals.css";
 import { ClerkProvider, currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import RightSidebar from "@/components/shared/RightSidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
